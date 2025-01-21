0b845984f2542ee8237aa9f6012b6a375b2946b4
lee <l_lpp@naver.com>
2025-01-21 15:10:08 +0900

    투스트링까지~~ 졸려 죽겟어요

--- src/main/java/himedia/phonebook/repository/vo/PhonebookVo.java
+++ src/main/java/himedia/phonebook/repository/vo/PhonebookVo.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 /*
  *  id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -17,6 +18,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 public class PhonebookVo {
 	private Integer id;
 	private String name;
